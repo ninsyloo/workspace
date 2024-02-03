d222fd27cb7f7a5bb398621eb8c6b637df6f7b0c
ninsyloo <evelinareal@gmail.com>
2024-02-03 20:18:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# workspace
+ Repasando Conceptos
---
+++ ultimatejs/01-tipos/01-variables.js
@@ -0,0 +1,20 @@
+let nombre = 'Shanshito';
+let NombreCompleto; //UpperCamelCase --> se utiliza para clases
+let nombreCompleto; //camelCase --> es por convencion la preferida
+let nombre_completo; //snake_case --> existe jaja
+
+console.log(nombre);
+
+let apellido, animal; //otra forma de declarar variables aunque x buenas practicas es preferible declararlas siempre en lineas independientes.
+
+apellido='Gordette'
+
+//tipos de datos
+
+//Primitivos         De Referencia
+
+//Number             Array
+//String             Object
+//Boolean            Function
+//Undefined          Classes
+//Null
