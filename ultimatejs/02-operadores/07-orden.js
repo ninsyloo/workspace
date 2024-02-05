3f65170b73873ad699928b7b677e782d9037cbbf
ninsyloo <evelinareal@gmail.com>
2024-02-05 00:27:51 -0300

    ternarios

---
+++ ultimatejs/02-operadores/07-orden.js
@@ -0,0 +1,11 @@
+/* let resul= 8/2(2+2); -->2 is not a funct err.
+console.log(resul);
+
+let result= 8/2*(2+2); -->16
+console.log(result); */
+
+let resultado = 8/(2*(2+2));
+console.log(resultado);//-->1
+
+//el orden en el cual se van a ejecutar los operadores esta marcado por los parentesis
+//al rededor de la operacion que quieras que se ejecute primero.
---
+++ ultimatejs/02-operadores/08-ternario.js
@@ -0,0 +1,5 @@
+// expresion ? 'si es true' : 'si es false'
+let edad = 25;
+let acceso = edad > 17 ? 'permitir ingreso' : 'no puede ingresar';
+
+console.log(acceso);
